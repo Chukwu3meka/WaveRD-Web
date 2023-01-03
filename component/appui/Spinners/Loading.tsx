2f4c2f8a39a2ca79832ad06015aeade291a8c967
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-03 07:17:43 +0100

    renamed spinner to loading

--- component/appui/Spinners/Loading.tsx
+++ component/appui/Spinners/Loading.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 import styles from "./styles.module.scss";
 
-const Spinner = ({ height }: { height?: number }) => (
-  <div className={styles.spinner} style={{ height: height ? "100%" : "100vh" }}>
+const Loading = ({ height }: { height?: number }) => (
+  <div className={styles.loading} style={{ height: height ? "100%" : "100vh" }}>
     <div>
       <Image src="/images/layout/ball.png" alt="SoccerMASS Advert" layout="fill" />
     </div>
   </div>
 );
 
-export default Spinner;
+export default Loading;
