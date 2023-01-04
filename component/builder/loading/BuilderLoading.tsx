c32323a43b66570b2b8f41e15ce3671ad456073f
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-04 05:12:52 +0100

    updated props in loader

--- component/builder/loading/BuilderLoading.tsx
+++ component/builder/loading/BuilderLoading.tsx
@@ -6,7 +6,16 @@ import { IBuilderLoading } from "@interface/builder/loading-interface";
 const BuilderLoading = ({ height, status, colorScheme, component }: IBuilderLoading) =>
   status ? (
     <div className={styles.loading} style={{ height: height || "100vh" }}>
-      <Image src="/images/layout/ball.png" alt="SoccerMASS Advert" height={50} width={50} />
+      <Image
+        src="/images/layout/ball.png"
+        alt="SoccerMASS Loading ball"
+        height={50}
+        width={50}
+        priority={true}
+        placeholder="blur"
+        blurDataURL="/images/layout/ball.png"
+        quality={100}
+      />
       <div>
         {new Array(5).fill("").map((_, index) => (
           <span key={index} style={{ backgroundColor: colorScheme[index] }} className={styles["loading-block"]}></span>
