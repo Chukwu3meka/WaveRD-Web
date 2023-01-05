03b1279708106c29a03349d2248fa3c73bf0381b
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-05 13:36:26 +0100

    added animation to loading

--- component/builder/loading/BuilderLoading.tsx
+++ component/builder/loading/BuilderLoading.tsx
@@ -1,21 +1,25 @@
-import { Fragment } from "react";
 import Image from "next/image";
 
 import { styles } from ".";
 import { IBuilderLoading } from "@interface/builder/loading-interface";
+import { Fade } from "react-awesome-reveal";
 
 const BuilderLoading = ({ height, loading, colorScheme, component }: IBuilderLoading) => (
   <>
-    {/* <main className={`${styles.loading} ${styles[loading ?"loading":"loading-hidden"]}`} style={{ height: height || "100%", display: loading ? "initial" : "none" }}> */}
-    <main className={styles[loading ? "loading" : "loading-hidden"]} style={{ height: height || "100%" }}>
-      <Image src="/images/layout/ball.png" alt="SoccerMASS Loading ball" height={50} width={50} placeholder="blur" blurDataURL="/images/layout/ball.png" />
-      <div>
-        {new Array(5).fill("").map((_, index) => (
-          <span key={index} style={{ backgroundColor: colorScheme[index] }} className={styles["loading-block"]}></span>
-        ))}
-      </div>
-    </main>
-    <main style={{ display: loading ? "none" : "initial" }}>{component}</main>
+    <Fade>
+      <main className={styles[loading ? "loading" : "loading-hidden"]} style={{ height: height || "100%" }}>
+        <Image src="/images/layout/ball.png" alt="SoccerMASS Loading ball" height={50} width={50} placeholder="blur" blurDataURL="/images/layout/ball.png" />
+        <div>
+          {new Array(5).fill("").map((_, index) => (
+            <span key={index} style={{ backgroundColor: colorScheme[index] }} className={styles["loading-block"]}></span>
+          ))}
+        </div>
+      </main>
+    </Fade>
+
+    <Fade direction="down" duration={2000} style={{ display: loading ? "none" : "initial" }}>
+      {component}
+    </Fade>
   </>
 );
 
