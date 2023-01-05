499eb1caa44c461f19687ff319908aabbff02d4c
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-05 05:43:56 +0100

    refactored app and document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
 import createEmotionCache from "@source/createEmotionCache";
 import createEmotionServer from "@emotion/server/create-instance";
-import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
@@ -9,13 +10,9 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-
           <link href="https://fonts.googleapis.com/css2?family=Merienda&amp;family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
-
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
-
-          {/* {this.props.emotionStyleTags} */}
         </Head>
         <body>
           <Main />
@@ -29,28 +26,6 @@ export default class MyDocument extends Document {
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   const originalRenderPage = ctx.renderPage;
 
   // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
@@ -65,14 +40,6 @@ MyDocument.getInitialProps = async (ctx) => {
           return <App emotionCache={cache} {...props} />;
         },
     });
-
-  // ctx.renderPage = () =>
-  //   originalRenderPage({
-  //     enhanceApp: (App) =>
-  //       function EnhanceApp(props) {
-  //         return <App emotionCache={cache} {...props} />;
-  //       },
-  //   });
 
   const initialProps = await Document.getInitialProps(ctx);
   // This is important. It prevents emotion to render invalid HTML.
