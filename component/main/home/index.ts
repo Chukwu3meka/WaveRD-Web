694e929da149d4a7aec7d11836c250940a3cdf8b
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-06 05:46:46 +0100

    removed HomeContainer in main/home

--- component/main/home/index.ts
+++ component/main/home/index.ts
@@ -1,8 +1,7 @@
-import HomeContainer from "./HomeContainer";
 import Home from "./Home";
 import Intro from "./Intro";
 import ApiHubIntro from "./ApiHubIntro";
 import ManagerIntro from "./ManagerIntro";
 import styles from "./styles.module.scss";
 
-export { styles, ApiHubIntro, Intro, ManagerIntro, Home, HomeContainer as default };
+export { styles, ApiHubIntro, Intro, ManagerIntro, Home as default };
