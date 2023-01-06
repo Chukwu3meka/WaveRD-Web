15490ff9c03f7b184627f55bc9e40ec1bf37f636
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-06 05:35:25 +0100

    removed loading border

--- component/builder/loading/BuilderLoading.tsx
+++ component/builder/loading/BuilderLoading.tsx
@@ -6,7 +6,7 @@ import { Fade } from "react-awesome-reveal";
 
 const BuilderLoading = ({ height, loading, colorScheme, component }: IBuilderLoading) => (
   <>
-    <div style={{ display: loading ? "unset" : "none", border: "3px solid red" }}>
+    <div style={{ display: loading ? "unset" : "none" }}>
       <Fade duration={2000} style={{ height: "100%" }}>
         <main className={styles.loading} style={{ height: height || "100%" }}>
           <Image src="/images/layout/ball.png" alt="SoccerMASS Loading ball" height={50} width={50} placeholder="blur" blurDataURL="/images/layout/ball.png" />
@@ -19,7 +19,7 @@ const BuilderLoading = ({ height, loading, colorScheme, component }: IBuilderLoa
       </Fade>
     </div>
 
-    <main style={{ display: loading ? "none" : "unset", border: "3px solid red" }}>
+    <main style={{ display: loading ? "none" : "unset" }}>
       <Fade direction="down" duration={2000}>
         {component}
       </Fade>
