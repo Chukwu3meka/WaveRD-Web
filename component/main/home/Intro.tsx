ac97143ba904d16d7b685b61a88b4c8cd1861f14
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-09 07:35:23 +0100

    fixed prod error from using external link in Link, and updated font-size

--- component/main/home/Intro.tsx
+++ component/main/home/Intro.tsx
@@ -1,21 +1,27 @@
-import Link from "next/link";
+import a from "next/link";
 import Typography from "@mui/material/Typography";
 
 import { styles } from ".";
 
 const Intro = () => (
   <div className={styles.intro}>
-    <Typography variant="caption" fontSize="1em">
-      <Link href={process.env.NEXT_PUBLIC_BASE_CLIENT!}>Welcome to</Link>
+    <Typography variant="caption" fontSize=".89em">
+      <a href={process.env.NEXT_PUBLIC_BASE_CLIENT!} rel="noopener noreferrer" target="_blank">
+        Welcome to
+      </a>
     </Typography>
-    <Typography variant="h1" fontSize="3em" fontWeight={900}>
+    <Typography variant="h1" fontSize="2.7em" fontWeight={900}>
       SoccerMASS
     </Typography>
-    <Typography variant="body2" fontSize="1.2em">
-      <Link href={process.env.NEXT_PUBLIC_BASE_MANAGER!}>The home of Soccer Managers</Link>
+    <Typography variant="body2" fontSize="1.05em">
+      <a href={process.env.NEXT_PUBLIC_BASE_MANAGER!} rel="noopener noreferrer" target="_blank">
+        The home of Soccer Managers
+      </a>
     </Typography>
-    <Typography variant="subtitle1" fontSize="1em">
-      <Link href={process.env.NEXT_PUBLIC_BASE_APIHUB!}>No. 1 Football API Provider</Link>
+    <Typography variant="subtitle1" fontSize=".89em">
+      <a href={process.env.NEXT_PUBLIC_BASE_APIHUB!} rel="noopener noreferrer" target="_blank">
+        No. 1 Football API Provider
+      </a>
     </Typography>
   </div>
 );
