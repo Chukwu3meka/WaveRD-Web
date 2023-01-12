30f9ad97cd557c8e8689b24a455c42b84ce43078
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-12 12:17:17 +0100

    added image bg to intro

--- component/main/home/Intro.tsx
+++ component/main/home/Intro.tsx
@@ -5,20 +5,20 @@ import { styles } from ".";
 
 const Intro = () => (
   <div className={styles.intro}>
-    <Typography variant="caption" fontSize=".89em">
+    <Typography variant="caption" fontSize={{ sm: ".89em", md: "3em", lg: "2.5em" }}>
       <a href={process.env.NEXT_PUBLIC_BASE_CLIENT!} rel="noopener noreferrer" target="_blank">
         Welcome to
       </a>
     </Typography>
-    <Typography variant="h1" fontSize="2.7em" fontWeight={900}>
+    <Typography variant="caption" fontSize={{ sm: ".89em", md: "3em", lg: "4em" }} fontWeight={900}>
       SoccerMASS
     </Typography>
-    <Typography variant="body2" fontSize="1.05em">
+    <Typography variant="body2" fontSize={{ sm: ".89em", md: "3em", lg: "1.5em" }}>
       <a href={process.env.NEXT_PUBLIC_BASE_MANAGER!} rel="noopener noreferrer" target="_blank">
         The home of Soccer Managers
       </a>
     </Typography>
-    <Typography variant="subtitle1" fontSize=".89em">
+    <Typography variant="subtitle1" fontSize={{ sm: ".89em", md: "3em", lg: "1.5em" }}>
       <a href={process.env.NEXT_PUBLIC_BASE_APIHUB!} rel="noopener noreferrer" target="_blank">
         No. 1 Football API Provider
       </a>
