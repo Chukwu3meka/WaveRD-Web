0636f6da75678f4ca7e25e1a85d0c9249383fc07
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-12 15:47:48 +0100

    updated header style

--- component/main/header/Header.tsx
+++ component/main/header/Header.tsx
@@ -1,96 +1,97 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Typography } from "@mui/material";
+import { Box, Button, IconButton, Stack, Typography } from "@mui/material";
 import { Fade } from "react-awesome-reveal";
 
 import { styles } from ".";
-import { IFooter } from "@interface/main/footer-interface";
+// import { IFooter } from "@interface/main/header-interface";
 import thirdPartyAccounts from "@source/thirdPartyAccounts";
+import { IThirdPartyAccounts } from "@interface/source/thirdPartyAccounts-interface";
 
-const Header = ({ logoutHandler, authenticated }: IFooter) => (
-  <Fade direction="right">
-    <div className={styles.footer}>
-      <footer>
-        <main>
-          <aside>
-            <Typography fontWeight={700} fontSize="2.2em" component="h1">
-              SoccerMASS
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const SocialIcon = ({ account, link }) => {
+  let icon;
+
+  switch (account) {
+    case "Whatsapp":
+      icon = <WhatsAppIcon fontSize="inherit" />;
+      break;
+    case "Twitter":
+      icon = <TwitterIcon fontSize="inherit" />;
+      break;
+
+    case "Instagram":
+      icon = <InstagramIcon fontSize="inherit" />;
+      break;
+    default:
+      return <></>;
+  }
+
+  return (
+    <IconButton LinkComponent={Link} color="inherit" rel="noopener noreferrer" href={link} target="_blank" sx={{ fontSize: "16px" }}>
+      {icon}
+    </IconButton>
+  );
+};
+
+// const Header = ({ logoutHandler, authenticated }: IFooter) => (
+const Header = ({ logoutHandler, authenticated }: any) => (
+  <Fade direction="down">
+    <div className={styles.header}>
+      <header>
+        <div>
+          <Image src="/images/soccermass.webp" alt="SoccerMASS" width={25} height={25} placeholder="blur" blurDataURL="/images/soccermass.webp" />
+          <Stack direction="row">
+            <Typography fontWeight={700} fontSize="1.7em" component="h1">
+              Soccer
             </Typography>
+            <Typography fontWeight={700} fontSize="1.7em" component="h1" color="primary">
+              MASS
+            </Typography>
+          </Stack>
+        </div>
 
-            <Image src="/images/soccermass.webp" alt="SoccerMASS" width={80} height={80} placeholder="blur" blurDataURL="/images/soccermass.webp" />
+        <div>
+          <Typography variant="subtitle2">Manager</Typography>
+          <Typography variant="subtitle2">API Hub</Typography>
+          <Typography variant="subtitle2">Home</Typography>
+        </div>
 
-            <Typography variant="subtitle2">Follow US</Typography>
-
-            <div>
-              {thirdPartyAccounts.map(([accounts, link = "https://alienforest.com/"]) => (
-                <a href={link} key={accounts} rel="noopener noreferrer" target="_blank">
-                  <Image src={`/images/social/${accounts.toLowerCase()}.png`} alt={`SoccerMASS ${accounts} page`} width={30} height={30} />
-                </a>
+        <div>
+          <div>
+            {thirdPartyAccounts
+              .filter((acc) => ["Twitter", "Instagram", "Whatsapp"].includes(acc[0]))
+              .map(([account, link = "https://alienforest.com/"]) => (
+                <SocialIcon account={account} link={link} />
               ))}
-            </div>
-          </aside>
+          </div>
 
-          <nav>
-            <div>
-              <label htmlFor="info">Info</label>
-              <Link href="/info/privacy/">Privacy Policy</Link>
-              <Link href="/info/contact/">Contact US</Link>
-              <Link href="/info/terms/">Terms &amp; Conditions</Link>
-              <Link href="/info/advertise/">Advertise</Link>
-              <Link href="/info/donate/">Support/Donation</Link>
-            </div>
-
-            <div>
-              <label htmlFor="soccermass-links">SoccerMASS</label>
-              <Link href="/">Home</Link>
-              <Link href="/apihub">API HUB</Link>
-              <Link href="/manager">Manager</Link>
-              {!authenticated && <Link href="/auth/signin">Signin</Link>}
-              {!authenticated && <Link href="/auth/signup">Signup</Link>}
-              <Link href="/auth/reset">Reset Password</Link>
-              {authenticated && (
-                <Link href="/auth/signin" onClick={logoutHandler()}>
-                  Logout
-                </Link>
-              )}
-            </div>
-
-            <div>
-              <label htmlFor="company">Company</label>
-              <a href="https://viewcrunch.com/" rel="noopener noreferrer">
-                ViewCrunch
-              </a>
-              <a href="https://soccermass.com/" rel="noopener noreferrer">
-                SoccerMASS
-              </a>
-              <a href="https://alienforest.com/" rel="noopener noreferrer">
-                AlienForest
-              </a>
-            </div>
-          </nav>
-        </main>
-
-        <section>
-          <Typography component="span" variant="body2" fontSize=".8em">
-            Proudly hosted on&nbsp;
-            <a href="https://vercel.com/" rel="noopener noreferrer" target="_blank">
-              Vercel
-            </a>
-            &nbsp;&&nbsp;
-            <a href="https://render.com/" rel="noopener noreferrer" target="_blank">
-              Render
-            </a>
-          </Typography>
-
-          <Typography component="span" variant="body2" fontSize=".9em">
-            ● All rights reserved. All trademarks are the property of their respective owners ●
-          </Typography>
-
-          <Typography component="span" variant="body2" fontSize="1em">
-            ©SoccerMASS 2018 ~ {new Date().getFullYear()}
-          </Typography>
-        </section>
-      </footer>
+          {!authenticated && (
+            <Link href="/auth/signin">
+              <Button variant="outlined" size="small">
+                Signin
+              </Button>
+            </Link>
+          )}
+          {!authenticated && (
+            <Link href="/auth/signup">
+              <Button variant="contained" size="small" color="primary">
+                Signup
+              </Button>
+            </Link>
+          )}
+          {authenticated && (
+            <Link href="/auth/signin" onClick={logoutHandler()}>
+              <Button variant="outlined" size="small">
+                Logout
+              </Button>
+            </Link>
+          )}
+        </div>
+      </header>
     </div>
   </Fade>
 );
