97afdd7ea294771327a909e298adf74232007dcc
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-12 18:36:31 +0100

    updated style for breakdown

--- component/main/home/Breakdown.tsx
+++ component/main/home/Breakdown.tsx
@@ -4,8 +4,16 @@ const Breakdown = () => {
   return (
     <div className={breakdownStyles.breakdown}>
       <div>
-        {/*  */}
-        Breakdown
+        <div>
+          <i>●●●💗●●●</i>
+          <span> No Player hoarding </span>
+          <span> Competitive transfer market </span>
+          <span> Advanced tactics and formation </span>
+          <span> Real match simulation </span>
+          <span> and many more</span>
+          <i>●●●💗●●●</i>
+        </div>
+        <img src="/images/layout/intro-breakdown.png" />
       </div>
     </div>
   );
