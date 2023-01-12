48a56ae2fbddcd95d7430e5b0c5a23a3d6129694
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-12 23:10:59 +0100

    updated styles for transparent

--- component/main/home/Home.tsx
+++ component/main/home/Home.tsx
@@ -1,16 +1,11 @@
-import { Fade } from "react-awesome-reveal";
 import { ApiHubIntro, Intro, ManagerIntro, Leagues, styles, Transparent } from ".";
 
 const NoAuthHome = () => (
   <>
-    {/* <div className={styles.home}> */}
-    {/* <Fade direction="down" duration={2000}> */}
-
     <Intro />
     <ManagerIntro />
     <Transparent />
-    {/* <ApiHubIntro /> */}
-    {/* </div> */}
+    <ApiHubIntro />
   </>
 );
 
