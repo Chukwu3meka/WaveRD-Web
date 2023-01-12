77ad4f3295a9c8aecde8ff5fbbd77ae057b033eb
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-13 00:36:09 +0100

    deleted leagues from main home

--- component/main/home/index.ts
+++ component/main/home/index.ts
@@ -1,11 +1,10 @@
 import Home from "./Home";
 import Intro from "./Intro";
 import ApiHubIntro from "./ApiHubIntro";
-import Leagues from "./Leagues";
 import Peaks from "./Peaks";
 import ManagerIntro from "./ManagerIntro";
 import styles from "./styles.module.scss";
 import leaguesStyles from "./leagues.module.scss";
 import peaksStyles from "./transparent.module.scss";
 
-export { Peaks, peaksStyles, styles, ApiHubIntro, Leagues, Intro, ManagerIntro, leaguesStyles, Home as default };
+export { Peaks, peaksStyles, styles, ApiHubIntro, Intro, ManagerIntro, leaguesStyles, Home as default };
