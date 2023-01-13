1192b6f909046fc4326cf10a6b6e250421af81ad
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-13 01:36:18 +0100

    updated apihub intro styles

--- component/main/home/ApiHubIntro.tsx
+++ component/main/home/ApiHubIntro.tsx
@@ -8,33 +8,14 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 const ApiHubIntro = () => (
-  <div className={apiHubIntroStyles.noAuthHome}>
+  <div className={apiHubIntroStyles.apihub}>
     <div>
-      <div>
-        <i>●●●💗●●●</i>
-        <span> No Player hoarding </span>
-        <span> Competitive transfer market </span>
-        <span> Advanced tactics and formation </span>
-        <span> Real match simulation </span>
-        <span> and many more</span>
-        <i>●●●💗●●●</i>
-      </div>
-    </div>
-    <div>
-      {/* {[
-        // "bundesliga 2.png",
-        "bundesliga.png",
-        "serie a tim.png",
-        // "calcio b.png",
-        "ligue 1 conforama.png",
-        // "domino's ligue 2.png",
-        "premier league.png",
-        // "efl championship.png",
-        "laliga santander.png",
-        // "laliga smartbank.png",
-      ].map((league) => (
-        <Image src={`/images/layout/${league}`} alt={`SoccerMASS ${league}`} width={80} height={80} key={league} />
-      ))} */}
+      <main>
+        {/* <Typography>{title}</Typography>
+      <Typography textAlign="justify">{body} </Typography> */}
+        <Link href="/apihub">See all features</Link>
+      </main>
+      <figure>{/* <Image src="/images/layout/intro-clubs.jpg" alt="" layout="fill" /> */}</figure>
     </div>
   </div>
 );
