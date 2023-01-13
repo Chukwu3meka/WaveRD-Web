0259f0f9dab38f10eca5b791a0156da707236810
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-13 15:34:14 +0100

    bug-fix
    fixing deployement error

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
  * @type {import('next').NextConfig}
  */
 
-const domains = [
-  { host: "localhost", domain: "http://localhost:3000" },
-  { host: "soccermass.com", domain: "https://soccermass.com" },
-  { host: "127.0.0.1", domain: "http://127.0.0.1:3000" },
-];
-
 const nextConfig = {
   /* config options here */
 
@@ -20,26 +14,6 @@ const nextConfig = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     ignoreBuildErrors: false,
-  },
-
-  async redirects() {
-    return [
-      // apihub
-      ...domains.map(({ host, domain }) => ({
-        source: "/:path*",
-        has: [{ type: "host", value: `apihub.${host}` }],
-        destination: `${domain}/apihub/:path*`,
-        permanent: false,
-      })),
-
-      // manager
-      ...domains.map(({ host, domain }) => ({
-        source: "/:path*",
-        has: [{ type: "host", value: `manager.${host}` }],
-        destination: `${domain}/manager/:path*`,
-        permanent: false,
-      })),
-    ];
   },
 };
 
