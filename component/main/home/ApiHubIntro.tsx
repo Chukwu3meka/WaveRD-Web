beb3d9c329a384ab235b4bca10c370dc206a1ab8
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-16 11:28:30 +0100

    updated  apihub intro style

--- component/main/home/ApiHubIntro.tsx
+++ component/main/home/ApiHubIntro.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Paper, Button, Typography, Stack, IconButton } from "@mui/material";
+import { Paper, Button, Typography, Stack, IconButton, Box } from "@mui/material";
 
 import VpnLockIcon from "@mui/icons-material/VpnLock";
 import DataObjectIcon from "@mui/icons-material/DataObject";
@@ -10,57 +10,67 @@ import { apiHubIntroStyles } from ".";
 
 const ApiHubIntro = () => (
   <div className={apiHubIntroStyles.apihub}>
-    <Stack direction="row" spacing={2} justifyContent="space-evenly">
-      <Paper elevation={4}>
-        <Image src="/images/layout/intro-apihub.jpg" alt="SoccerMASS API HUB welcome image" layout="fill" />
-      </Paper>
+    <Stack direction="row" spacing={3} justifyContent="space-evenly">
+      <div>
+        <Image src="/images/layout/intro-apihub.png" alt="SoccerMASS API HUB welcome image" layout="fill" />
+      </div>
 
       <main>
-        <Typography>- WHY DO ANYTHING?</Typography>
-        <Typography textAlign="justify">Maintaining Football data is not core to your businesss</Typography>
+        <Typography color="primary">- WHY DO ANYTHING?</Typography>
 
-        <Stack direction="row">
-          <IconButton>
-            <NetworkCheckIcon />
-          </IconButton>
+        <Box sx={{ display: "flex", flexWrap: "wrap", marginY: 2 }}>
+          <Typography fontSize="2em" fontWeight={700}>
+            Maintaining Football data is
+          </Typography>
+          <Typography fontSize="2em" fontWeight={700} color="primary">
+            &nbsp;not core&nbsp;
+          </Typography>
+          <Typography fontSize="2em" fontWeight={700}>
+            to your businesss
+          </Typography>
+        </Box>
 
-          <Stack>
-            <Typography fontWeight={600}>Improve Performance</Typography>
-            <Typography>Our Server is hosted on a reliable Cloud Provider to ensure 24/7 data availability and minimal response time with little to no down time</Typography>
-          </Stack>
-        </Stack>
+        <div>
+          {apihubFeatures.map(({ icon, title, description }) => (
+            <Stack direction="row" alignItems="flex-start" key={title} spacing={2} marginBottom={3}>
+              <IconButton color="primary" sx={{ fontSize: "25px", marginTop: -0.5 }}>
+                {icon}
+              </IconButton>
 
-        <Stack direction="row">
-          <IconButton>
-            <DataObjectIcon />
-          </IconButton>
+              <Stack>
+                <Typography fontWeight={800}>{title}</Typography>
+                <Typography>{description}</Typography>
+              </Stack>
+            </Stack>
+          ))}
+        </div>
 
-          <Stack>
-            <Typography fontWeight={600}>Massive data to fetch</Typography>
-            <Typography>
-              Building and maintaining ever changing football data will stretch your budget and Team. Allow SoccerMASS handle this task as we provide a large pool of data to fetch
-              from ranging from Players, Clubs, Countries, Leagues, e.t.c.
-            </Typography>
-          </Stack>
-        </Stack>
-
-        <Stack direction="row">
-          <IconButton>
-            <VpnLockIcon />
-          </IconButton>
-
-          <Stack>
-            <Typography fontWeight={600}>Create Robust App</Typography>
-            <Typography>
-              First to provide a full-text-search powered by the most powerful NoSQL Database 'MongoDB'. Overwhelming functionalities to consume, enough to solve your data worries.
-            </Typography>
-          </Stack>
-        </Stack>
-
-        <Link href="/apihub">See all features</Link>
+        <Link href="/apihub">
+          <Typography>See all features</Typography>
+        </Link>
       </main>
     </Stack>
   </div>
 );
 
 export default ApiHubIntro;
+
+const apihubFeatures = [
+  {
+    icon: <NetworkCheckIcon fontSize="inherit" />,
+    title: "Improve Performance",
+    description: "Our Server is hosted on a reliable Cloud Provider to ensure 24/7 data availability and minimal response time with little to no down time",
+  },
+  {
+    icon: <DataObjectIcon fontSize="inherit" />,
+    title: "Massive data to consume",
+    description:
+      "Building and maintaining ever changing football data will stretch your budget and Team. Allow SoccerMASS handle this task as we provide a large pool of data to fetch from ranging from Players, Clubs, Countries, Leagues, e.t.c.",
+  },
+  {
+    icon: <VpnLockIcon fontSize="inherit" />,
+    title: "Create Robust App",
+    description:
+      "First to provide a full-text-search powered by the most powerful NoSQL Database 'MongoDB'. Overwhelming functionalities to consume, enough to solve your data worries.",
+  },
+];
