2bbaa479af5e291d8698e26ed4cbd20f51bd7efd
Chukwuemeka Maduekwe <chukwuemeka@alienforest.com>
2023-01-25 00:02:06 +0100

    removed comments in createemotioncache.ts and added authSlideText

---
+++ source/constants/authSlideText.tsx
@@ -0,0 +1,57 @@
+export default [
+  <>
+    <h5>It's great to see you again</h5>
+    <h2>Your'e officially welcome to SoccerMASS</h2>
+    <h3>It's time to roll your sleeves</h3>
+    <h4>Signin and let's begin</h4>
+  </>,
+
+  <>
+    <h2>We are happy to have you, SoccerMASS was built to meet your need of real soccer experience</h2>
+    <h3>
+      <ul>
+        <li>Interactive Play</li>
+        <li>Realistic Transfer &amp; Player's Condition</li>
+        <li>Major League &amp; Competitions</li>
+      </ul>
+    </h3>
+  </>,
+
+  <>
+    <h1>World of Managers</h1>
+    <h2>Take your team to the peak</h2>
+    <h3>Tactics &amp; Performance</h3>
+  </>,
+
+  <>
+    <h2>Why SoccerMASS</h2>
+    <h3>
+      <ul>
+        <li>Free forever</li>
+        <li>No. 1 soccer manager</li>
+        <li>Highly Interactive</li>
+        <li>Competitive transfer market</li>
+        <li>No player hoarding</li>
+      </ul>
+    </h3>
+  </>,
+
+  <>
+    <h2>We are happy to have you onboard</h2>
+    <h4>We promise the best, and to keep you up to date, with the best tips on how to rule the game and win trophies</h4>
+    <h3>Complete registration to explore and the universal home to soccer managers</h3>
+  </>,
+
+  <>
+    <h2>Your'e in</h2>
+    <h3>We'll be in touch shortly!</h3>
+    <h4>***FOR THE BOLD AND AWESOME***</h4>
+    <h3>Your players are hungry to see you take charge, click "CONTINUE" to feed their flames</h3>
+  </>,
+
+  <>
+    <h2>Password Reset</h2>
+    <h3>A verification mail wait an OTP will be sent to your mail.</h3>
+    <h4>Copy and paste the OTP to verify ownership.</h4>
+  </>,
+];
