b8462ec2e5f72523670d6492d12a5d3c7ea5d4da
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-01 20:56:14 +0100

    added regex validator for handle and full name

--- utils/validator.ts
+++ utils/validator.ts
@@ -1,6 +1,6 @@
 interface IValidator {
   value: any;
-  type: "email" | "password";
+  type: "email" | "password" | "handle";
   label: string;
 }
 
@@ -9,14 +9,26 @@ const validator = ({ value, type, label }: IValidator) => {
 
   switch (type) {
     case "email": {
-      const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+      // ? <= email address that starts with a word or number
+      const reg = /^[\w\d]+[\w\.-]+@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
       if (!reg.test(value)) throw { message: `Invalid ${label}` };
       break;
     }
-
     case "password": {
-      // To check a password between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character
+      // ? <= To check a password between 8 to 15 characters which contain at least one lowercase/uppercase, numeric digit, and special character
       const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+      if (!reg.test(value)) throw { message: `Invalid ${label}` };
+      break;
+    }
+    case "handle": {
+      // ? <= start with a letter or number and only contain letters, numbers, or underscores
+      const reg = /^[a-zA-Z0-9_]+$/;
+      if (!reg.test(value)) throw { message: `Invalid ${label}` };
+      break;
+    }
+    case "handle": {
+      // ? <= start with a letter, and allows for middle names or initials separated by spaces, dots, single quotes, or hyphens.
+      const reg = /^[a-zA-Z]+((['. -][a-zA-Z ])?[a-zA-Z]*)*$/;
       if (!reg.test(value)) throw { message: `Invalid ${label}` };
       break;
     }
