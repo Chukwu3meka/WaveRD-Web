34b4a3b0db49c0f9e1d24bd8a67eac6a202741c7
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-01 21:32:40 +0100

    added single error message to regex validator

--- utils/validator.ts
+++ utils/validator.ts
@@ -5,20 +5,19 @@ interface IValidator {
 }
 
 const validator = ({ value, type, label }: IValidator) => {
-  if (!value) throw { message: "value not defined" };
+  if (value === "" || value === undefined) throw { message: `${label} cannot be empty` };
 
   switch (type) {
     case "email": {
       const reg = /^[\w\d]+[\w\.-]+@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-      if (!reg.test(value)) throw { message: `Invalid ${label}`, description: "email address that starts with a letter or number" };
+      if (!reg.test(value)) throw { message: `${label} email address that starts with a letter or number` };
       break;
     }
     case "password": {
       const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
       if (!reg.test(value))
         throw {
-          message: `Invalid ${label}`,
-          description: `${label} must be between 8 to 15 characters which contain at least one lowercase/uppercase, numeric digit, and special character`,
+          message: `${label} must be between 8 to 15 characters which contain at least one lowercase/uppercase, numeric digit, and special character`,
         };
       break;
     }
@@ -26,8 +25,7 @@ const validator = ({ value, type, label }: IValidator) => {
       const reg = /^[a-zA-Z0-9]+(_[a-zA-Z0-9]+)?$/;
       if (!reg.test(value))
         throw {
-          message: `Invalid ${label}`,
-          description: `${label} must start with a letter or number and can only have underscore in between letters or numbers`,
+          message: `${label} must start with a letter or number and can only have underscore in between letters or numbers`,
         };
       break;
     }
@@ -35,8 +33,7 @@ const validator = ({ value, type, label }: IValidator) => {
       const reg = /^[a-zA-Z]+([\ \'\.\-][a-zA-Z]+)*$/;
       if (!reg.test(value))
         throw {
-          message: `Invalid ${label}`,
-          description: `${label} can have one or more letters, and zero or more occurrences of a dash, dot, space, or hyphen followed by one or more letters.`,
+          message: `${label} can have one or more letters, and zero or more occurrences of a dash, dot, space, or hyphen followed by one or more letters.`,
         };
       break;
     }
