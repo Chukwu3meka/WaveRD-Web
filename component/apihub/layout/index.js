9913926ac4c5a70f125ce546aca7ed60f966e2c5
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-02-22 01:31:17 +0100

    bug fix
    fixed layout import error in app

--- component/apihub/layout/index.js
+++
@@ -1,6 +0,0 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Layout from "./Layout";
-import styles from "./styles.module.scss";
-
-export { styles, Header, Footer, Layout as default };
