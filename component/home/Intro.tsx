714f0b22f78bbe8aa23f9e4ae7ee050f670d2735
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-03-30 11:56:08 +0100

    Bug Fix && Improvement
    updated header style

--- component/home/Intro.tsx
+++ component/home/Intro.tsx
@@ -12,19 +12,19 @@ const Intro = () => (
 
       <div>
         <div>
-          <Typography fontSize={{ xs: "1.5em", sm: "1.89em", md: "1.5em", lg: "3em" }}>Welcome to</Typography>
+          <Typography fontSize={{ xs: "1.4em", sm: "1.89em", md: "2.7em", lg: "3.3em", xl: "4em" }}>Welcome to</Typography>
 
-          <Typography fontSize={{ xs: "2.8em", sm: "2.89em", md: "2.7em", lg: "5em" }} fontWeight={900}>
-            <a href={process.env.NEXT_PUBLIC_BASE_CLIENT!} rel="noopener noreferrer" target="_blank">
+          <Typography fontSize={{ xs: "2.7em", sm: "3.89em", md: "4.0em", lg: "5em", xl: "6em" }} fontWeight={900}>
+            <a href="/organization" rel="noopener noreferrer" target="_blank">
               SoccerMASS
             </a>
           </Typography>
 
-          <Typography fontSize={{ xs: "1.1em", sm: "1.3em", md: "1.5em", lg: "1.2em" }}>
+          <Typography fontSize={{ xs: "1.1em", sm: "1.3em" }}>
             The ultimate destination for football enthusiasts and managers, designed to provide you with everything you need to stay ahead of the game.
           </Typography>
 
-          <Typography fontSize={{ xs: "1.1em", sm: "1.3em", md: "1.5em", lg: "1.2em" }} mt="10px" mb="20px">
+          <Typography fontSize={{ xs: "1.1em", sm: "1.3em" }} mt="10px" mb="20px">
             With our cutting-edge tools and in-depth insights, you can access real-time data. So join us today and experience the power of data-driven football
             API like never before!
           </Typography>
