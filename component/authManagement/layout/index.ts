f983149ef5b4e214c96d2ed52a9bc936a0f12d0c
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-04-18 05:59:32 +0100

    Bug Fix
    move auth management to accounts and auth should be pointing to accounts

--- component/authManagement/layout/index.ts
+++
@@ -1,5 +0,0 @@
-import Layout from "./Layout";
-import LayoutContainer from "./LayoutContainer";
-import styles from "./styles.module.scss";
-
-export { Layout, styles, LayoutContainer as default };
--- pages/accounts/[route].tsx
+++ pages/accounts/[route].tsx
@@ -1,23 +1,29 @@
-import dynamic from "next/dynamic";
+import Head from "next/head";
 import ErrorPage from "next/error";
+import dynamic from "next/dynamic";
+import { connect } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Carousel from "nuka-carousel/lib/carousel";
 
-import LayoutContainer from "@component/authManagement/layout";
-import Head from "next/head";
-import { capitalizeFirstLetter } from "@utils/handlers";
+import { logoutAction } from "@store/actions";
+import { RelativeHeader } from "@component/layout/header";
+import authSlideText from "@source/constants/authSlideText";
+import styles from "@source/scss/accountStyles.module.scss";
 
 const Page = () => {
   const router = useRouter(),
     { route } = router.query,
     [component, setComponent] = useState(<></>),
-    [invalidRoute, setInvalidRoute] = useState(false);
+    [invalidRoute, setInvalidRoute] = useState(false),
+    [pageTitle, setPageTitle] = useState<string>("");
 
   useEffect(() => {
     // ? Verify that user has visited a valid auth route
-    if (["emailConfirmation", "forgotPassword", "signin", "signup"].includes(route as string)) {
-      const AuthPage = dynamic(() => import(`@component/authManagement/${route}`));
+    if (validRoutes.map(({ path }) => path).includes(route as string)) {
+      const AuthPage = dynamic(() => import(`@component/accounts/${route}`));
       setComponent(<AuthPage />);
+      setPageTitle(validRoutes.find(({ path }) => path === route)?.label || "");
     } else {
       setInvalidRoute(true);
     }
@@ -28,15 +34,41 @@ const Page = () => {
   ) : (
     <>
       <Head>
-        <title>SoccerMASS {capitalizeFirstLetter(route as string)}</title>
-        <meta name="description" content={`SoccerMASS ${capitalizeFirstLetter(route as string)}`} />
-        <meta property="og:description" content={`SoccerMASS ${capitalizeFirstLetter(route as string)}`} />
-        <meta name="keywords" content={`${route as string}, soccer manager, soccer, soccermass, football manager, football`} />
+        <title>SoccerMASS {pageTitle}</title>
+        <meta name="description" content={`SoccerMASS ${pageTitle}`} />
+        <meta property="og:description" content={`SoccerMASS ${pageTitle}`} />
+        <meta name="keywords" content={`${pageTitle.toLowerCase()}, soccer manager, soccer, soccermass, football manager, football`} />
       </Head>
 
-      <LayoutContainer component={component} />
+      <div className={styles.layout}>
+        <div>
+          <aside>
+            <div>
+              <Carousel wrapAround={true} slidesToShow={1} autoplay={true} withoutControls={true} adaptiveHeight={true} autoplayInterval={5000}>
+                {authSlideText.map((slideText: any) => (
+                  <div key={slideText}>{slideText}</div>
+                ))}
+              </Carousel>
+            </div>
+          </aside>
+          <main>
+            <RelativeHeader theme="light" />
+            {component}
+          </main>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Page;
+const mapStateToProps = (state: any) => ({ authenticated: state.auth.status }),
+  mapDispatchToProps = { logoutAction };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
+
+const validRoutes = [
+  { path: "emailConfirmation", label: "Email Confirmation" },
+  { path: "forgotPassword", label: "Forgot Password" },
+  { path: "signin", label: "Signin" },
+  { path: "signup", label: "Signup" },
+];
