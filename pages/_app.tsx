0d5da64121aa267b55431e7dacb159341076d235
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-04-27 17:48:52 +0100

    add vercel web analytics to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@source/scss/global-style.scss";
 import { useStore } from "@store/index";
@@ -7,7 +8,12 @@ import LayoutContainer from "@component/layout";
 const App = (props: AppProps) => {
   const store = useStore(props.pageProps.initialReduxState);
 
-  return <LayoutContainer {...{ ...props, store }} />;
+  return (
+    <>
+      <LayoutContainer {...{ ...props, store }} />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
