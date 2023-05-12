572f8ee316491045a1cdb8de862aee7a143d4ee7
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-12 21:26:15 +0100

    bug fix, optimization and new features
    remove scroll in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { ApiHub, Welcome, ManagerContainer } from "@component/home";
 
 export default () => (
-  <>
+  <main>
     <Welcome />
     <ManagerContainer />
     <ApiHub />
-  </>
+  </main>
 );
