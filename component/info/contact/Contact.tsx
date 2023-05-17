3d93679a79a161c0e610beb9c8887145fc708495
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-17 16:06:52 +0100

    bug fix, optimization and new features
    update contact us page

--- component/info/contact/Contact.tsx
+++ component/info/contact/Contact.tsx
@@ -1,66 +1,34 @@
-import { Grid, Paper, Select, Hidden, Button, MenuItem, TextField, Typography, InputLabel, FormControl, FormHelperText, Box } from "@mui/material";
+import { Grid, Paper, Select, Hidden, Button, MenuItem, TextField, Typography, InputLabel, FormControl, FormHelperText, Box, Stack } from "@mui/material";
 import { styles } from ".";
 import Link from "next/link";
 import Image from "next/image";
 
-const Section = ({ supportTeam, sectionHandler }) => (
-  <div>
-    {supportTeam.map(({ supportType, image, description, buttonType }) => (
-      <Paper elevation={2} key={supportType} onClick={() => sectionHandler({ target: { value: buttonType } })}>
-        <div>
-          <Image src={image} fill alt="SoccerMASS Contact Us" />
-        </div>
-
-        <Typography variant="h6">
-          <b>{supportType}</b>
-        </Typography>
-
-        <Typography variant="body2" color="textSecondary">
-          {description}
-        </Typography>
-      </Paper>
-    ))}
-  </div>
-);
-
 const Contact = ({ contactLinks, sectionHandler, setValues, values, submitHandler, commentRef, supportTeam }) => (
   <main className={styles.contact}>
-    <Typography gutterBottom variant="h4" component="h2">
-      We're are here to help
-    </Typography>
-    <hr />
-    <Section supportTeam={supportTeam} sectionHandler={sectionHandler} />
-    <Grid item container alignItems="center">
-      <Grid item container xs={12} md={12}>
-        <Grid item xs={12} md={8}>
-          <Typography variant="h4">Contact Support</Typography>
-          <Typography>
-            Already using ViewCrunch? Please, SignIn by clicking the Facebook signin button at the buttom of this page; So we can tailor your support
-            experience. If that's not possible, we'll still like to hear from you.
+    <div className={styles.categories}>
+      {supportTeam.map(({ supportType, image, description, buttonType }) => (
+        <Paper elevation={2} key={supportType} onClick={() => sectionHandler({ target: { value: buttonType } })}>
+          <div>
+            <Image src={image} fill alt="SoccerMASS Contact Us" />
+          </div>
+
+          <Typography variant="h6">
+            <b>{supportType}</b>
           </Typography>
-        </Grid>
-        <Grid item xs={12} md={4} />
-      </Grid>
-      <Grid item container xs={12} md={12}>
-        <Grid item xs={12} md={8}>
-          <Paper elevation={4}>
-            <div>
-              <Typography gutterBottom variant="body2" component="span">
-                Visit a link
-              </Typography>
-              <section>
-                {contactLinks.map(({ title, path }) => (
-                  <Link href={path} key={title}>
-                    <Button color="secondary" variant="outlined">
-                      {title}
-                    </Button>
-                  </Link>
-                ))}
-              </section>
-            </div>
-            <FormHelperText>Choose what best describes your reason for reaching out</FormHelperText>
+
+          <Typography variant="body2" color="textSecondary">
+            {description}
+          </Typography>
+        </Paper>
+      ))}
+    </div>
+
+    <Paper elevation={2} className={styles.contactForm}>
+      <Stack spacing={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
             <FormControl fullWidth>
-              <InputLabel id="section-id">Section</InputLabel>
+              <InputLabel id="section-id">Category</InputLabel>
               <Select labelId="section-id" id="section" value={values.section} onChange={sectionHandler} label="Section">
                 <MenuItem value="others">Others</MenuItem>
                 <MenuItem value="advertising">Advertising</MenuItem>
@@ -69,22 +37,8 @@ const Contact = ({ contactLinks, sectionHandler, setValues, values, submitHandle
                 <MenuItem value="service">Digital Service</MenuItem>
               </Select>
             </FormControl>
-            <TextField
-              fullWidth
-              label="Comment"
-              variant="outlined"
-              value={values?.comment}
-              inputRef={commentRef}
-              placeholder={values?.section === "service" ? "What product will you like us to work on?" : "What will you like to tell us?"}
-              onChange={(e) => setValues({ ...values, comment: e.target.value })}
-            />
-            <TextField
-              fullWidth
-              label="Name"
-              value={values?.name}
-              placeholder="How do you want our respondents to address you"
-              onChange={(e) => setValues({ ...values, name: e.target.value })}
-            />
+          </Grid>
+          <Grid item xs={12} md={4}>
             <TextField
               fullWidth
               label="eMail"
@@ -92,20 +46,35 @@ const Contact = ({ contactLinks, sectionHandler, setValues, values, submitHandle
               placeholder="We'll reach out to you soon"
               onChange={(e) => setValues({ ...values, email: e.target.value })}
             />
-            <Button color="primary" variant="contained" onClick={submitHandler}>
-              Submit
-            </Button>
-          </Paper>
-        </Grid>
-        <Hidden mdDown>
-          <Grid item xs={12} md={4}>
-            <div>
-              <Image src="/images/ViewCrunch.webp" alt="SoccerMASS Contact Us" fill />
-            </div>
           </Grid>
-        </Hidden>
-      </Grid>
-    </Grid>
+          <Grid item xs={12} md={4}>
+            <TextField
+              fullWidth
+              label="Name"
+              value={values?.name}
+              placeholder="How do we address you"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+          </Grid>
+        </Grid>
+
+        <TextField
+          fullWidth
+          label="Comment"
+          variant="outlined"
+          value={values?.comment}
+          minRows={5}
+          multiline
+          inputRef={commentRef}
+          placeholder={values?.section === "service" ? "What product will you like us to work on?" : "What will you like to tell us?"}
+          onChange={(e) => setValues({ ...values, comment: e.target.value })}
+        />
+
+        <Button color="primary" variant="contained" onClick={submitHandler}>
+          Submit
+        </Button>
+      </Stack>
+    </Paper>
   </main>
 );
 
--- component/info/contact/ContactContainer.tsx
+++ component/info/contact/ContactContainer.tsx
@@ -4,6 +4,7 @@ import { useState, useRef, useEffect } from "react";
 
 import { Contact } from ".";
 import { Router, useRouter } from "next/router";
+import ComingSoon from "@component/shared/comingSoon";
 
 const contactLinks = [
   { title: "ViewCrunch Services", path: "/company" },
@@ -16,42 +17,29 @@ const contactLinks = [
 const supportTeam = [
   {
     supportType: "Technical Support",
-    image: "/images/info/technicalSupport.png",
-    description: "Already using ViewCrunch and experiencing issues on our platform",
+    image: "/images/layout/support.png",
+    description: "Already using SoccerMASS and experiencing issues on our platform",
     buttonType: "technical",
   },
   {
     supportType: "Advertising",
-    image: "/images/info/advertising.png",
+    image: "/images/layout/advertise.png",
     description: "Having a pricing question or need help managing your ads",
     buttonType: "advertising",
   },
   {
     supportType: "Others",
-    image: "/images/info/others.png",
-    description: "Evaluating our service or issue not related to Technical support or Advertising",
+    image: "/images/layout/others.png",
+    description: "Evaluating our service? Not related to Technical support or Advertising",
     buttonType: "others",
   },
 ];
 
-const ContactContainer = (props) => {
+export default (props) => {
   const router = useRouter(),
     commentRef = useRef(null),
     { enqueueSnackbar } = useSnackbar(),
-    [online, setOnline] = useState(props.online),
     [values, setValues] = useState({ email: "", name: "", comment: "", section: "others" });
-
-  useEffect(() => {
-    setOnline(props.online);
-  }, [props.online]);
-
-  useEffect(() => {
-    if (router.query.section === "service") {
-      setValues({ ...values, section: "service" });
-      // scroll to comment on section change
-      setTimeout(() => commentRef.current.focus(), 100);
-    }
-  }, []);
 
   const sectionHandler = ({ target: { value } }) => {
     setValues({ ...values, section: value });
@@ -61,26 +49,21 @@ const ContactContainer = (props) => {
 
   const submitHandler = async () => {
     if (values.name && values.email && values.comment) {
-      if (online) {
-        // const status = await fetcher("/api/info/contactus", values);
-        // if (!status) return enqueueSnackbar(`Please, Try again. Server unable to handle request.`, { variant: "error" });
-
-        setValues({ email: "", name: "", comment: "", section: "others" });
-        enqueueSnackbar(`Submitted successful, We'll get in touch soon`, { variant: "success" });
-      } else {
-        enqueueSnackbar(`Network connectivity issue.`, { variant: "warning" });
-      }
+      // const status = await fetcher("/api/info/contactus", values);
+      // if (!status) return enqueueSnackbar(`Please, Try again. Server unable to handle request.`, { variant: "error" });
+      // setValues({ email: "", name: "", comment: "", section: "others" });
+      // enqueueSnackbar(`Submitted successful, We'll get in touch soon`, { variant: "success" });
+      // } else {
+      //   enqueueSnackbar(`Network connectivity issue.`, { variant: "warning" });
+      // }
     } else {
       enqueueSnackbar(`All fields are mandatory.`, { variant: "warning" });
     }
   };
 
-  return <Contact {...{ contactLinks, sectionHandler, setValues, values, submitHandler, commentRef, supportTeam }} />;
+  return process.env.NODE_ENV === "production" ? (
+    <ComingSoon header={true} />
+  ) : (
+    <Contact {...{ contactLinks, sectionHandler, setValues, values, submitHandler, commentRef, supportTeam }} />
+  );
 };
-
-const mapStateToProps = (state) => ({
-    online: state?.device?.online,
-  }),
-  mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactContainer);
