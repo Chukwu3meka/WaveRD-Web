ef02943af393893ed83c2f561982f1e12233d6f4
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-17 13:43:46 +0100

    bug fix, optimization and new features
    add partial to auth payload in store

--- store/reducers/auth.ts
+++ store/reducers/auth.ts
@@ -2,7 +2,7 @@ import { AuthState } from "@interface/store/auth";
 
 const initialState = null;
 
-const authReducer = (state: AuthState = initialState, { payload, type }: { payload: AuthState; type: string }) => {
+const authReducer = (state: AuthState = initialState, { payload, type }: { payload: Partial<AuthState>; type: string }) => {
   switch (type) {
     case "SET_AUTH":
       return payload;
