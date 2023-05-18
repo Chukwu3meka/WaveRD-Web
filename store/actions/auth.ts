0c16e6dbb2fce5c10f989b2407dbe0b34e07569a
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-18 20:56:14 +0100

    bug fix, optimization and new features
    update info layout style and header style

--- store/actions/auth.ts
+++ store/actions/auth.ts
@@ -25,7 +25,7 @@ export const signoutAction = () => async (dispatch: AppDispatch) => {
 };
 
 export const verifyCookieAction = (payload: any) => async (dispatch: AppDispatch) => {
-  document.documentElement.style.setProperty("--headerHeight", "71px");
+  document.documentElement.style.setProperty("--headerHeight", "75px");
   document.documentElement.style.setProperty("--visibleScreen", `${window.innerHeight + 0}px`); // <= iPhone not returning the right screen height in VH
 
   try {
