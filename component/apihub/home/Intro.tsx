118c7a394d435e8c3c988f8e9ff60c2b56a8c832
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-23 11:57:25 +0100

    bug fix, optimization and new features
    inti intro screen for api hub

--- component/apihub/home/Intro.tsx
+++ component/apihub/home/Intro.tsx
@@ -1,25 +1,35 @@
-import { Box, Grid, Typography, Button } from "@mui/material";
+import { Box, Grid, Typography, Button, Stack } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import { introStyles as styles } from ".";
 
 export default () => (
-  <Grid container spacing={2}>
-    <Grid item xs={12} md={8}>
-      <Typography color="whitesmoke">ONLINE FOOTBALL API</Typography>
-      <Typography color="whitesmoke">Connect with your community</Typography>
-      <Typography color="whitesmoke">Showcase your unique brand and engage with local families through a bold and easy-to-manage API.</Typography>
-      <Link href="/documentation">
-        <Button size="large" variant="contained" color="success">
-          Start Building
-        </Button>
-      </Link>
-    </Grid>
+  <div className={styles.intro}>
+    <Grid container spacing={2} alignItems="center" overflow="hidden">
+      <Grid item xs={12} md={6}>
+        <Stack spacing={2} p={5}>
+          <Typography fontSize="2em">SoccerMASS API Hub</Typography>
+          <Typography fontSize=".9em" color="primary">
+            Connect with your community
+          </Typography>
+          <Typography fontSize="1.1em">
+            Showcase your unique brand and engage with your users through a bold and easy-to-manage API. We offer Player, Club, Country, Competition, Referee
+            and many more APIs
+          </Typography>
 
-    <Grid item xs={0} md={4}>
-      <figure className={styles.intro}>
-        <Image src="/images/layout/apihub-intro.png" alt="SoccerMASS API Hub" fill />
-      </figure>
+          <Link href="/apihub/documentation">
+            <Button size="large" variant="contained" color="success">
+              View Documentation
+            </Button>
+          </Link>
+        </Stack>
+      </Grid>
+
+      <Grid item md={6} display={{ xs: "none", sm: "none", md: "initial" }}>
+        <figure className={styles.image}>
+          <Image src="/images/layout/apihub-intro.png" alt="SoccerMASS API Hub" fill />
+        </figure>
+      </Grid>
     </Grid>
-  </Grid>
+  </div>
 );
