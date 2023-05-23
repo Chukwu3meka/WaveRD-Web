99ac353b3c3a337fd5f662dc6d9294369789d4f9
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-05-23 19:40:02 +0100

    bug fix, optimization and new features
    add endpoint component to apihub

--- component/apihub/endpoints/index.tsx
+++ component/apihub/endpoints/index.tsx
@@ -1,4 +1,8 @@
-import EndpointsContainer from "./EndpointsContainer";
+import EndpointContainer from "./endpoint/EndpointContainer";
+import NavContainer from "./nav/NavContainer";
+import Endpoint from "./endpoint/Endpoint";
 import Endpoints from "./Endpoints";
+import Nav from "./nav/Nav";
+import styles from "./endpoint.module.scss";
 
-export { Endpoints, EndpointsContainer as default };
+export { styles, Endpoint, EndpointContainer, Nav, NavContainer, Endpoints as default };
--- pages/apihub/endpoints.tsx
+++ pages/apihub/endpoints.tsx
@@ -1,6 +1,5 @@
+import Endpoints from "@component/apihub/endpoints";
 import ComingSoon from "@component/shared/comingSoon";
-import { Intro, Peaks } from "@component/apihub/home";
-import { Endpoints } from "@component/apihub/endpoints";
 
 const Page = () =>
   process.env.NODE_ENV === "production" ? (
