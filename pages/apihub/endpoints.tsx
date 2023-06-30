ed298b6457fbf15e502c77ab2b7416485737102b
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2023-07-01 00:26:32 +0100

    show endpoints in production, update intro screen

--- pages/apihub/endpoints.tsx
+++ pages/apihub/endpoints.tsx
@@ -1,13 +1,5 @@
 import Endpoints from "@component/apihub/endpoints";
-import ComingSoon from "@component/shared/comingSoon";
 
-const Page = () =>
-  process.env.NODE_ENV === "production" ? (
-    <main>
-      <ComingSoon header={false} minHeight="calc(var(--visibleScreen) - var(--headerHeight))" />
-    </main>
-  ) : (
-    <Endpoints />
-  );
+const Page = () => <Endpoints />;
 
 export default Page;
