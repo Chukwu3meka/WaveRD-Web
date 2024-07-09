85821ec3aea252261f5bc4da2e46bbb46e78c2dc
Chukwuemeka Maduekwe <chukwuemeka@soccermass.com>
2024-07-09 15:52:30 +0100

    fix deployment error

--- app/console/games/modify-world/page.tsx
+++ app/console/games/modify-world/page.tsx
@@ -7,15 +7,16 @@ const EndpointsPage = async () => {
   const cookie = await getUserCookies(),
     consoleService = new ConsoleService();
 
-  const endpoint = await consoleService
-    .getEndpoint({ id, cookie })
-    .then(({ success, data }) => {
-      if (success) return data;
-      return null;
-    })
-    .catch(() => null);
+  // const endpoint = await consoleService
+  //   .getEndpoint({ id, cookie })
+  //   .then(({ success, data }) => {
+  //     if (success) return data;
+  //     return null;
+  //   })
+  //   .catch(() => null);
 
-  return <ConsoleEndpointContainer endpoint={endpoint} exists={id !== "new"} />;
+  // return <ConsoleEndpointContainer endpoint={endpoint} exists={id !== "new"} />;
+  return <p>ss</p>;
 };
 
 export default EndpointsPage;
